fafe7d573375cfbf916a0f2ffc05fa2a389210d7
Infinite.sx <ops@shift.sx>
2016-04-24 22:12:59 -0300

    Added files via upload

---
+++ monitors/vm/google/config.js
@@ -0,0 +1,12 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'm777483515-74ff1e6f2e6b88abd5f7e40c',
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
+
+// the default language
+// set false to disable language support and show only english,
+// or set null to use visitor's browser language
+var __language = null;
